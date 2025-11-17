7d1e76a9f95ad3cde9d51a0ad2347390751c98cc
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-11-17 20:44:29 +0900

    ✨ Feat: 모노레포용 CI/CD 변경

---
+++ apps/api/scripts/docker/test-cicd.sh
@@ -0,0 +1,14 @@
+# docker buildx build \
+#   --platform linux/amd64 \
+#   -t my-test-image:amd64 \
+#   -f apps/api/scripts/docker/dockerfile \
+#   --load \
+#   .
+
+ENV_FILE=""
+
+IMAGE_NAME="my-test-image:amd64"
+CONTAINER_NAME="my-test-container"
+
+docker run --rm --env-file "$ENV_FILE" -p 8080:9999 \
+--name "$CONTAINER_NAME" "$IMAGE_NAME"
