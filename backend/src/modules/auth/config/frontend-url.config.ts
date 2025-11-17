2cde70dc7ea40fd85b6e4bb52d2a1478a6e931fb
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-11-17 21:16:06 +0900

    [Release] v1.0.0 (#45)
    * ♻️ Refactor: 모노레포를 위해 디렉토리 이관 및 prettier 설정 통합
    
    * ♻️ Refactor: package 파일 정리 및 모노레포 설정 완료
    
    * 📦️ Package: script 수정
    
    * ✨ Feat: 모노레포용 CI/CD 변경
    
    * ♻️ Refactor: 불용 config code 및 dotenv 정리

--- backend/src/modules/auth/config/frontend-url.config.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export const FRONTEND_URL_CONFIG = Symbol('FRONTEND_URL_CONFIG');
-
-export const FrontendUrlConfig = registerAs(FRONTEND_URL_CONFIG, () => ({
-  localUrl: process.env.FRONTEND_LOCAL_URL as string,
-  devUrl: process.env.FRONTEND_DEVELOPMENT_URL as string,
-  prodUrl: process.env.FRONTEND_PROD_URL as string,
-}));
